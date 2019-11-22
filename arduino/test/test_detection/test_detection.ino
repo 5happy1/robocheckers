a1a5e1a8b1dae4f05c485a031f17d039d929bd84
David Miron <david.miron@duke.edu>
2019-11-22 18:30:56 -0500

    Work on test detection

--- arduino/test/test_detection/test_detection.ino
+++ arduino/test/test_detection/test_detection.ino
@@ -1,46 +1,80 @@
-int pin = A0;
-int state = 0;
-int next_state = 0;
-unsigned long time_difference_started = 0;
+const int num_pins = 16;
 
-float threshold_upper = 3;
-float threshold_lower = 2;
+// Pin information
+int pins[num_pins] = {A0, A1, A2, A3, A4, A5, A6, A7,
+                     A8, A9, A10, A11, A12, A13, A14, A15 };
+float upper_thresholds[num_pins];
+float lower_thresholds[num_pins];
+//
+//const A1 = 0;
+//const A3 = 
+//const A5 = 
+//const A7 = 
+
+// State information
+int states[num_pins];
+int next_states[num_pins];
+unsigned long times_differences_started[num_pins];
+
+// Constants
+float upper_threshold_default = 3;
+float lower_threshold_default = 2;
 
 // If state is changed for this amount of time, the state is officially changed
-float time_for_new_state = 500;
+float time_for_new_state = 400;
 
 void setup() {
+
+  // Initialize variables
+  for(int i = 0; i < num_pins; i++) {
+
+    // Thresholds
+    upper_thresholds[i] = upper_threshold_default;
+    lower_thresholds[i] = lower_threshold_default;
+
+    // State information
+    states[i] = 0;
+    next_states[i] = 0;
+    times_differences_started[i] = 0;
+  }
+  
+
+  // Initialize Serial
   Serial.begin(9600);
-  Serial.print("Starting state: ");
-  Serial.println(state);
 }
 
 void loop() {
 
-  int new_state = state_of_pin(pin);
+  for (int pin_index = 0; pin_index < num_pins; pin_index++) {
 
-  // If new state is different and the change is not yet recorded in next_state, set
-  // next_state and start a timer
-  if (new_state != state && new_state != next_state) {
-    next_state = new_state;
-    time_difference_started = millis();
-  }
-
-  if (next_state != state && millis() - time_difference_started > time_for_new_state) {
-    state = next_state;
-    Serial.print("State changed, now is ");
-    Serial.println(state);
+    int new_state = state_of_pin(pin_index);
+  
+    // If new state is different and the change is not yet recorded in next_state, set
+    // next_state and start a timer
+    if (new_state != states[pin_index] && new_state != next_states[pin_index]) {
+      next_states[pin_index] = new_state;
+      times_differences_started[pin_index] = millis();
+    }
+  
+    if (next_states[pin_index] != states[pin_index] &&
+        millis() - times_differences_started[pin_index] > time_for_new_state) {
+      states[pin_index] = next_states[pin_index];
+      Serial.print("Pin ");
+      Serial.print(pin_index);
+      Serial.print(" state changed, to ");
+      Serial.println(states[pin_index]);
+    }
   }
 
 }
 
-int state_of_pin(int pin) {
+int state_of_pin(int pin_index) {
   
-  float voltage = analogRead(pin) * (5.0 / 1024);
+  float voltage = analogRead(pins[pin_index]) * (5.0 / 1024);
 
-  if (voltage < threshold_lower) {
+  if (voltage < lower_thresholds[pin_index]) {
     return -1;
-  } else if (voltage > threshold_upper) {
+  } else if (voltage > upper_thresholds[pin_index]) {
     return 1;
   } else {
     return 0;
